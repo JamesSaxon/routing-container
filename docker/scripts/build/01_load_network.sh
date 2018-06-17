44c6ff34ec1edaf5bff7d0196b0fb46b9c2b749f
Dan Snow <dfsnow@uchicago.edu>
2018-06-17 02:43:41 -0500

    Added disconnected components handling and bugfix

--- docker/scripts/build/01_load_network.sh
+++ docker/scripts/build/01_load_network.sh
@@ -1,17 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
 
 
 if [ -f /scripts/input/*osm ]; then
 
+  sed -i 's/\\//g' *osm
   osm2pgrouting -U ${POSTGRES_USER:-postgres} -d ${POSTGRES_DB:-postgres} -f /scripts/input/*osm
 
-else 
+else
 
   BUFFER=${BUFFER:-0.01}
   coords=$(psql -U postgres -t -A -F"," -c "SELECT ROUND(MIN(y-$BUFFER)::numeric, 3) ymin, ROUND(MIN(x-$BUFFER)::numeric, 3) xmin, ROUND(MAX(y+$BUFFER)::numeric, 3) ymax, ROUND(MAX(x+$BUFFER)::numeric, 3) xmax FROM locations;")
 
   wget 'http://overpass-api.de/api/interpreter?data=(way["highway"~"road|motorway|trunk|primary|secondary|tertiary|residential|living_street|unclassified"]('${coords}');>;);out;' -O osm.osm
 
+  sed -i 's/\\//g' *osm
   osm2pgrouting -U ${POSTGRES_USER:-postgres} -d ${POSTGRES_DB:-postgres} -f osm.osm
 
 fi
