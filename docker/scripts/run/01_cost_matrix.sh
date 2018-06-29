78baf8b4fc2bcc060c05142c84ca2197213d66d2
Jamie Saxon <jsaxon@uchicago.edu>
2018-06-29 16:03:41 -0500

    cost matrix to rural

--- docker/scripts/run/01_cost_matrix.sh
+++ docker/scripts/run/01_cost_matrix.sh
@@ -1,21 +1,26 @@
 #!/bin/bash 
 
-BUFFER=0.025
-
 psql -U postgres -t -A -F"," -o /scripts/output/cost_matrix.csv -c "
 
   SELECT origins.id origin, destinations.id destination, agg_cost FROM pgr_dijkstraCost('
-    	WITH w AS (
+    	WITH
+        w AS (
     	    SELECT ST_Buffer(ST_Envelope(ST_Union(point)), 0.025) u 
-    	    FROM   locations
-    	  )
+    	    FROM   locations),
+        speeds AS (
+          SELECT 
+            tag_id,
+            CASE WHEN rural THEN maxspeed_rural 
+                            ELSE maxspeed_urban END maxspeed
+          FROM configuration, geoid, rural
+          WHERE rural.geoid = geoid.geoid ORDER BY tag_id)
     	SELECT
         gid id, source, target, 
         CASE WHEN cost         < 0 THEN 1e8 ELSE length_m * 6.2e-4 * 60 / maxspeed END AS cost,
         CASE WHEN reverse_cost < 0 THEN 1e8 ELSE length_m * 6.2e-4 * 60 / maxspeed END AS reverse_cost
       FROM ways
-      JOIN configuration ON 
-        ways.tag_id = configuration.tag_id
+      JOIN speeds ON 
+        ways.tag_id = speeds.tag_id
       WHERE ST_Intersects(the_geom, (SELECT u FROM w))
     ', 
     (SELECT array_agg(osm_nn) FROM locations WHERE dir != 1),
