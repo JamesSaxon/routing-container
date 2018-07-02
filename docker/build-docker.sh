1d1bb9c619b41e0645e42f3e03aa54f3760dd153
Jamie Saxon <jsaxon@uchicago.edu>
2018-07-02 11:37:09 -0500

    new speeds

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t routing .
+docker build --no-cache -t routing -f Dockerfile-AWS .
 $(aws ecr get-login --no-include-email --region us-east-1)
 docker tag routing:latest 808035620362.dkr.ecr.us-east-1.amazonaws.com/routing:latest
 docker push 808035620362.dkr.ecr.us-east-1.amazonaws.com/routing:latest
