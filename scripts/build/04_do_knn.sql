949a6f3dbed385aa860e6896c0fe6f48822a191c
Jamie Saxon <jsaxon@uchicago.edu>
2019-08-27 18:59:37 -0500

    do not start paths, on the highway --- even for driving

--- scripts/build/04_do_knn.sql
+++ scripts/build/04_do_knn.sql
@@ -1,6 +1,19 @@
 ALTER TABLE locations ADD COLUMN osm_nn    BIGINT;
 ALTER TABLE locations ADD COLUMN snap_dist FLOAT;
 
+ALTER TABLE ways_vertices_pgr ADD COLUMN hway BOOLEAN DEFAULT(FALSE);
+
+UPDATE ways_vertices_pgr
+SET hway = TRUE
+FROM ways 
+JOIN configuration ON 
+  configuration.tag_id = ways.tag_id
+WHERE
+  (ways.source_osm = ways_vertices_pgr.osm_id OR
+   ways.target_osm = ways_vertices_pgr.osm_id) AND
+  configuration.tag_value IN ('motorway', 'motorway_junction', 'motorway_link', 'trunk', 'trunk_link')
+;
+
 DROP FUNCTION IF EXISTS DoKnnMatch;
 CREATE FUNCTION DoKnnMatch(init_tol float8, max_tol float8, factor_tol float8)
 RETURNS float8 AS $$
@@ -17,14 +30,14 @@ BEGIN
              FROM (
                SELECT DISTINCT ON (loc.id)
                  loc.id, vtx.id vtx_id,
-		 ST_Distance(point::geography, the_geom::geography) vtx_dist
+		             ST_Distance(point::geography, vtx.the_geom::geography) vtx_dist
                FROM
                  locations loc, ways_vertices_pgr vtx
-               WHERE
-                 osm_nn IS NULL AND 
-                 ST_DWithin(point, the_geom, ' || tol || ')
+	             WHERE 
+                 NOT vtx.hway AND osm_nn IS NULL AND 
+                 ST_DWithin(point, vtx.the_geom, ' || tol || ')
                ORDER BY
-                 id, ST_Distance(point::geography, the_geom::geography)
+                 id, ST_Distance(point::geography, vtx.the_geom::geography)
              ) knn WHERE locations.id = knn.id;
              ';
 
